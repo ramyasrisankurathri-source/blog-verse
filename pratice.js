22c91224d9a5c0c24ebe63c66cf25906bab15d5c
ramyasri <ramyasrisankurathri@gmail.com>
2025-09-06 12:38:34 +0530

    hii sriii

--- pratice.js
+++ pratice.js
@@ -1,1 +1,29 @@
 console.log("hello world,sriii")
+let name = "dev astra";
+console.log(name)
+
+let num=18;
+console.log(num)
+let isologged=false;
+console.log(isologged)
+let x;
+console.log(x)
+
+let y=null;
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isologged)
+console.log(typeof x)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person ={
+name:"srii",
+age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
